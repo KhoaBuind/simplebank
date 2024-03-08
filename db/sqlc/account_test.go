c5bcd50355ac55df5b4ba51d954c618751ead009
Mike <khoa.bui@brickmate.vn>
2024-03-08 16:45:13 +0700

    update unit test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,26 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"simplebank/util"
+	"testing"
+)
 
-func testCreateAccount(t *testing.T) {
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    util.RandomOwer(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
 
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }
