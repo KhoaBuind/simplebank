34350d38f2b5ffa1b865de9d4421467d297e24e6
Mike <khoa.bui@brickmate.vn>
2024-03-11 17:09:12 +0700

    update unit test

--- Makefile
+++ Makefile
@@ -40,7 +40,7 @@ sqlc:
 	sqlc generate
 
 test:
-	go test -v -cover -short ./...
+	go test -v -cover ./...
 
 server:
 	go run main.go
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,9 +5,14 @@ import (
 	"github.com/stretchr/testify/require"
 	"simplebank/util"
 	"testing"
+	"time"
 )
 
 func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwer(),
 		Balance:  util.RandomMoney(),
@@ -23,4 +28,24 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
 }
