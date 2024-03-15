4b44ac162e7b1d17f4faa06f3a7658c8e7000dc9
Mike <khoa.bui@brickmate.vn>
2024-03-15 08:25:30 +0700

    update unit test

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,88 @@
+package db
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDb)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	// Run in concurrent transfer translation
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxtParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	// Check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		//check Transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		//Check Entry
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, account1.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		//Check Entry
+		ToEntry := result.ToEntry
+		require.NotEmpty(t, ToEntry)
+		require.Equal(t, account2.ID, ToEntry.AccountID)
+		require.Equal(t, amount, ToEntry.Amount)
+		require.NotZero(t, ToEntry.ID)
+		require.NotZero(t, ToEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), ToEntry.ID)
+		require.NoError(t, err)
+
+		//Check Account
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		ToAccount := result.ToAccount
+		require.NotEmpty(t, ToAccount)
+		require.Equal(t, account2.ID, ToAccount.ID)
+
+		//Check Account Balance
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := ToAccount.Balance - account2.Balance
+	}
+}
